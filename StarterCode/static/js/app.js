b3e711299503ab98aeaa79779e1cbe371b28e03c
Howard <g8r1fl@gmail.com>
2021-05-06 15:28:11 -0400

    copied samples.json as samples.js to experiment creating the plots.  I made the bar plot but it has all the otu_id's so i need to limit it to 10

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,22 @@
+console.log("This is from my samples.js file",samples);
+
 d3.json('samples.json').then(function(data) {
     console.log(data);
-    console.log(data[0]);
+    console.log(data.samples[0].otu_ids);
+    var samples = data.samples;
+    console.log(samples);
 });
+
+
+var trace1 = {
+    x: samples.samples[3].sample_values,
+    y: samples.samples[3].otu_ids,
+    text: samples.samples[3].otu_labels,
+    type: 'bar'
+};
+
+var data = [trace1];
+
+var layout = {};
+
+Plotly.newPlot("bar", data)
