9d626213cb59896ed863706064d490a7a8d00f60
Howard <g8r1fl@gmail.com>
2021-05-17 21:10:05 -0400

    trying to make loop to fill in key and value in demographic card

--- StarterCode/static/js/test.js
+++ StarterCode/static/js/test.js
@@ -16,23 +16,28 @@ d3.json('samples.json').then(data => {
     
     // filter samples to element 940
     let filtered = data.samples.filter(sample => sample.id === "940");
-    let x = filtered.map(otus => otus.otu_ids);
-    console.log("This is the mapped otu ids for ID #940: ",x[0].slice(0,10));
+    let y = filtered.map(otus => otus.otu_ids);
+    console.log("This is the mapped otu ids for ID #940: ",y[0].slice(0,10));
     console.log(filtered[0].otu_ids);
     //  get data for plots
     let sample = data.samples.filter(sample => sample.id === "940");
-    let x_buble = sample.otu_ids;
-    let y_bubble = sample.sample_values;
-    let x_bar = x.slice(0,10);
+    let x_bubble = sample[0].otu_ids;
+    let y_bubble = sample[0].sample_values;
+    let y_bar = y[0].slice(0,10).map(String);
+    y_bar = y_bar.map(el => "OTU "+el)
+    console.log(y_bar);
+    let x_bar = sample[0].sample_values.slice(0,10);
     console.log(x_bar);
-    let y_bar = sample[0].sample_values.slice(0,10);
     let droplist = data.names;
+    let demographic = data.metadata[0];
+    console.log(d3.keys(demographic));
     
     // build initial hbar plot
     var trace1 = {
-        x: x_bar,
-        y: y_bar,
-        type: 'bar'
+        x: x_bar.reverse(),
+        y: y_bar.reverse(),
+        type: 'bar',
+        orientation: 'h'
     };
     
     var data = [trace1];
@@ -40,13 +45,32 @@ d3.json('samples.json').then(data => {
     Plotly.newPlot("bar", data);
 
     // build initial bubble plot
+    var trace2 = {
+        x: x_bubble,
+        y: y_bubble,
+        mode: 'markers',
+        marker: {
+            size: sample[0].sample_values
+        }
+    };
+    
+    var data2 = [trace2];
 
+    Plotly.newPlot("bubble", data2);
     // build dropdown list
     const menu = d3.select("#selDataset");
 
     droplist.forEach(item => {
         menu.append("option").attr("value", item).text(item);
     })
+
+    // build metadata card
+    const meta = d3.select("#sample-metadata");
+    demographic.forEach((elem) => {
+        // meta.append("p").attr("class", "card-text").text(d3.keys(elem));
+        console.log(d3.keys(elem))
+    })
+
     // menu.on("change", () => 
     // updatePlot(data));
 });
